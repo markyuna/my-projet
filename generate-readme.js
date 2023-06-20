83a5d538a7f764e27910727213b6b17c5e25bb80
MARCOS <marcossuarezr88@gmail.com>
2023-06-20 14:59:48 +0200

    adding readme

--- generate-readme.js
+++ generate-readme.js
@@ -21,6 +21,6 @@ Descripción de tu proyecto.
 
 ## Resultado final
 
-![Imagen del resultado final](assets/logo.png)`;
+![Imagen del resultado final](assets/app.png)`;
 
 fs.writeFileSync('README.md', readmeContent);
