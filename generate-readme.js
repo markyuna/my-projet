1d0951c39a1441460d06fc29eb095f996636fc05
MARCOS <marcossuarezr88@gmail.com>
2023-06-20 14:59:16 +0200

    adding readme

--- generate-readme.js
+++ generate-readme.js
@@ -6,21 +6,18 @@ Descripción de tu proyecto.
 
 ## Versiones utilizadas
 
-"@react-native-community/masked-view": "^0.1.11",
-"@react-navigation/bottom-tabs": "^5.11.15",
-"@react-navigation/drawer": "^5.12.9",
-"@react-navigation/native": "^5.9.8",
-"@react-navigation/stack": "^5.14.9",
-"expo": "~48.0.18",
-"npm": "^9.7.1",
-"react": "18.2.0",
-"react-native": "0.71.8",
-"react-native-gesture-handler": "~2.9.0",
-"react-native-paper": "^5.8.0",
-"react-native-reanimated": "~2.14.4",
-"react-native-safe-area-context": "4.5.0",
-"react-native-screens": "~3.20.0",
-"react-native-vector-icons": "^9.2.0"
+- React: 18.2.0
+- react-native": 0.71.8
+- react-navigation: 4.4.4
+- react-navigation-drawer: 2.7.1
+- react-navigation-stack: 2.10.4
+- react-navigation-tabs: 2.11.1
+- react-native-gesture-handler: 1.10.3
+- react-native-reanimated: 2.2.4
+- react-native-safe-area-context: 3.3.2
+- react-native-screens: 3.8.0
+- react-native-vector-icons: 8.1.0
+
 
 ## Resultado final
 
